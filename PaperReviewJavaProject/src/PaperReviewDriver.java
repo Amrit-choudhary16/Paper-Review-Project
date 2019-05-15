8b020c17f6ed5916dda179b642b9600cafa76144
Amrit-choudhary16 <choudhary.amrit16@gmail.com>
2019-05-14 23:18:31 -0400

    Added updated scripts for schema changes

---
+++ PaperReviewJavaProject/src/PaperReviewDriver.java
@@ -0,0 +1,13 @@
+public class PaperReviewDriver {
+
+    public static void main(String[] args) {
+
+        getAuthorDetails("");
+
+        System.out.println("Hello World!");
+    }
+
+    private static void getAuthorDetails(String key){
+
+    }
+}
